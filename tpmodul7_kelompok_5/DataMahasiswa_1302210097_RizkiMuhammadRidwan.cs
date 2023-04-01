791e12a025b29236ef1009c2f83df19bc32f6080
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2023-04-02 01:00:29 +0700

    "menambahkan class DataMahasiswa_1302210097_RizkiMuhammadRidwan"

---
+++ tpmodul7_kelompok_5/DataMahasiswa_1302210097_RizkiMuhammadRidwan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace tpmodul7_kelompok_5
+{
+    internal class DataMahasiswa_1302210097_RizkiMuhammadRidwan
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText(@"E:\Tugas Kuliah\Semester 4\KPL\Praktikum\tpmodul7_1302210097\Zaky-Git-TP_KPL_MOD7_KELOMPOK_5\tpmodul7_kelompok_5\tp7_1_1302210097.json");
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine($"Nama {jsonObject.nama.depan} {jsonObject.nama.belakang} dengan nim {jsonObject.nim} dari fakultas {jsonObject.fakultas}");
+        }
+    }
+}
--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using tpmodul7_kelompok_5;
+
+DataMahasiswa_1302210097_RizkiMuhammadRidwan objMhs = new DataMahasiswa_1302210097_RizkiMuhammadRidwan();
+
+objMhs.ReadJSON();
