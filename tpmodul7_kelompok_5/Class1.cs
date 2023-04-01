4a1ff835c89801e5b7eb2766adf7d5f77707d3fd
Raychan69 <fasyaraihan82@gmail.com>
2023-04-01 23:59:33 +0700

    menambahkan class DataMahasiswa_1302213116

---
+++ tpmodul7_kelompok_5/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace tpmodul7_kelompok_5
+{
+
+    internal class DataMahasiswaFasyaRaihanMaulana
+    {
+        public void readJSON()
+        {
+          
+            string jsonString = File.ReadAllText(@"C:\Users\Raychan\Documents\KuliAhhh\Kuliah\Semester 4\Kontruksi Perangkat Lunak\Tugas Pendahuluan\Tugas Pendahuluan 7\Zaky-Git-TP_KPL_MOD7_KELOMPOK_5\tpmodul7_kelompok_5\tp7_1_1302213116.json");
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine($"Nama {jsonObject.nama.depan} {jsonObject.nama.belakang} dengan nim {jsonObject.nim} dari fakultas {jsonObject.fakultas}");
+        }
+    }
+}
--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using tpmodul7_kelompok_5;
+
+DataMahasiswaFasyaRaihanMaulana objMhs = new DataMahasiswaFasyaRaihanMaulana();
+
+objMhs.readJSON();
